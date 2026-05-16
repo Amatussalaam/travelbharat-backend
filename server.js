4df418b544f8157d03eb4642e9dee3f864e04d44
Amatus Salaam <webcare123@gmail.com>
2026-05-16 12:15:57 +0530

    Fix Cors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log("MongoDB Error ❌", err));
 // ================= MIDDLEWARE =================
 app.use(cors({
-    origin: "*",
+    origin:  ["https://your-netlify-site.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
