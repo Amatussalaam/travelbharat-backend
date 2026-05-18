e73fc55a301455a5d3ec273b4910c463630baaa3
Amatus Salaam <webcare123@gmail.com>
2026-05-18 12:52:48 +0530

    working

--- server.js
+++ server.js
@@ -30,12 +30,22 @@ app.use(cors({
     credentials: true
 }));
 
-app.options("/*", cors());
 // ✅ HANDLE PREFLIGHT PROPERLY
 app.use((req, res, next) => {
     if (req.method === "OPTIONS") {
         return res.sendStatus(200);
     }
+    next();
+});
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
     next();
 });
 app.use(express.json());
