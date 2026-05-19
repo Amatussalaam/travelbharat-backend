d8b962db5fb1d9880d9a76c2594916e809a5d019
Amatus Salaam <webcare123@gmail.com>
2026-05-19 16:59:51 +0530

    debug

--- server.js
+++ server.js
@@ -16,10 +16,19 @@ const SECRET = process.env.JWT_SECRET;
 const app = express();
 
 // ================= MONGODB CONNECT =================
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected ✅"))
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("MongoDB Connected ✅");
+})
+.catch(err => {
+    console.log("MongoDB Error ❌");
+    console.log(err);
+});
 
-.catch(err => console.log("MongoDB Error ❌", err));
+console.log("MONGO URI:", process.env.MONGO_URI);
 // ================= MIDDLEWARE =================
 app.use(cors({
     origin: [
